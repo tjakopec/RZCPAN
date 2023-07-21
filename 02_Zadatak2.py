f7f0fdd30108e790b03d3e35c77f63fac4b2618d
Tomislav Jakopec <tjakopec@gmail.com>
2023-07-21 09:28:41 +0200

    MVP rješenje zadatka

---
+++ 02_Zadatak2.py
@@ -0,0 +1,9 @@
+
+# Program unosi dva cijela broja
+# Program ispisuje sve brojeve između dva
+# unesena broja
+
+b1 = int(input('Unesi prvi broj: '))
+b2 = int(input('Unesi drugi broj: '))
+for i in range(b1,b2+1):
+    print(i)
