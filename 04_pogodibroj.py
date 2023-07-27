f15c96ce82e1fd2083a6f4a6cc82a756a86b4432
Tomislav Jakopec <tjakopec@gmail.com>
2023-07-27 10:43:01 +0200

    Dodavanje ispiosa broja pokušaja

--- 04_pogodibroj.py
+++ 04_pogodibroj.py
@@ -3,9 +3,10 @@ import random as r
 import zajednicki_modul as m
 
 sb = r.randint(1,100)
-
+pokusaji=0
 while True:
     b = m.ulaz('Pogodi broj: ',int)
+    pokusaji=pokusaji+1
     if sb == b:
         print('Čestitam')
         break
@@ -14,3 +15,4 @@ while True:
             print('Zamišljeni broj je veći')
         else:
             print('Zamišljeni broj je manji')
+print('Pogodili ste iz',pokusaji,'. pokušaja')
