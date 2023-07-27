7ac66277c7d25f895793a698fcbad52f8cd9ad3b
Tomislav Jakopec <tjakopec@gmail.com>
2023-07-27 10:55:47 +0200

    Prikaz brojeva koji su pogađani

--- 04_pogodibroj.py
+++ 04_pogodibroj.py
@@ -4,13 +4,16 @@ import zajednicki_modul as m
 
 sb = r.randint(1,100)
 pokusaji=0
+pogadali=[]
 while True:
     b = m.ulaz('Pogodi broj: ',int)
     pokusaji=pokusaji+1
+    pogadali.append(b)
     if sb == b:
         print('Čestitam')
         break
     else:
+        print('Do sada si probao',pogadali,sep='\n')
         if b < sb:
             print('Zamišljeni broj je veći')
         else:
